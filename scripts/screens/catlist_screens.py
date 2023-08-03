337ef911064e9e95b956b024c457cecbc60c3e47
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2023-08-03 12:04:08 -0500

    Update catlist_screens.py

--- scripts/screens/catlist_screens.py
+++ scripts/screens/catlist_screens.py
@@ -340,7 +340,7 @@ class ClanScreen(Screens):
                 else:
                     continue
  
-            if Cat.all_cats[x].status in ['apprentice', 'mediator apprentice', "queen's apprentice"]:
+            if Cat.all_cats[x].status in ['apprentice', 'mediator apprentice']:
                 Cat.all_cats[x].placement = self.choose_nonoverlapping_positions(first_choices, all_dens,
                                                                                  [1, 50, 1, 1, 100, 100, 1])
             elif Cat.all_cats[x].status == 'deputy':
