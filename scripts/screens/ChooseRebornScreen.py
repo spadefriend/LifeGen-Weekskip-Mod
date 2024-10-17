8adf1c878b55a1034b8217bf554ae3f8dc33338f
spadefriend <trash0751@gmail.com>
2024-10-16 18:40:27 -0700

    Update ChooseRebornScreen.py

--- scripts/screens/ChooseRebornScreen.py
+++ scripts/screens/ChooseRebornScreen.py
@@ -253,7 +253,7 @@ class ChooseRebornScreen(Screens):
 
             info = self.selected_cat.status + "\n" + \
                    self.selected_cat.genderalign + "\n" + self.selected_cat.personality.trait + "\n"
-            if self.selected_cat.moons < 1:
+            if self.selected_cat.moons < 5:
                 info += "???"
             else:
                 info += self.selected_cat.skills.skill_string(short=True)
